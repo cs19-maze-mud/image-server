5abf0b3f5c4fb67ca0f0e168715e62efe61aa211
dan2014 <dmen326@gmail.com>
2019-07-31 12:03:05 -0400

    updated index.js to receive port from environment variable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const cors = require("cors")
-var fs = require('fs');
 
 
 
@@ -8,19 +7,9 @@ const server = express()
 
 server.use(cors())
 
-// server.use(express.static('images'))
-
-
-// server.get("/", (req,res)=>{
-//     res.write(bg,'binary');
-//     res.end(null, 'binary');
-// })
-
-
-// server.get("/pokemon_terrain.jpg", (req,res)=>{
-//     res.sendFile("/Users/dan/Documents/Lambda_School/Lambda_Projects/Maze_Mud/image_server/base64/base64_terrain")
-// })
 
 server.use(express.static('images'))
 
-server.listen(5000,()=> console.log("listening on port 5000"))
+const port = process.env.PORT || 5000
+
+server.listen(port,()=> console.log(`listening on port ${port}`))
